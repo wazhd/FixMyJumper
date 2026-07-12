da893a024a3446f06cad52aba6157664378ca635
wazhd <157824420+minecraftMiner23@users.noreply.github.com>
2026-07-12 16:32:41 -0400

    update

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ with open("instructions.txt", "r") as f:
     instructions = f.read()
 
 mp_pose = mp.solutions.pose
-
+pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 mp_drawing = mp.solutions.drawing_utils
 
 
